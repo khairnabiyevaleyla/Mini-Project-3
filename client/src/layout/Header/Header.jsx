c7807b5deb580d8440af9db83317933a6f6a1019
Leyla Khairnabiyeva <leylaskh@code.edu.az>
2025-03-31 16:02:07 +0400

    feat: add additional navigation links to the header

--- client/src/layout/Header/Header.jsx
+++ client/src/layout/Header/Header.jsx
@@ -4,13 +4,9 @@ import style from "./style.module.scss";
 
 const Header = () => {
   const LeftLinks = [
-    {
-      name: "Home",
-      url: "/",
-    },
+    { name: "Home", url: "/" },
     { name: "Shop", url: "/products" },
     { name: "Product", url: "/products" },
-    { name: "New In", url: "/products" },
     { name: "Blog", url: "/blog" },
   ];
 
@@ -21,27 +17,50 @@ const Header = () => {
   ];
 
   return (
-    <header
-      className="
-    bg-black"
-    >
+    <header className={style.container}>
       <nav>
-        <div className="grid grid-cols-3">
-          <div>
-            {" "}
-            <ul className={style.leftLinks}>
-              {LeftLinks.map((link) => (
-                <li key={link.name} className={style.links}>
+        <div className="grid grid-cols-4 gap-5 justify-center items-center">
+          {/* Left Links */}
+          <ul className={style.links}>
+            {LeftLinks.map((link) => (
+              <li key={link.name} className={style.linksitem}>
+                <Link to={link.url}>{link.name}</Link>
+              </li>
+            ))}
+          </ul>
+
+          {/* Logo */}
+          <Link to="/">
+            <div className={style.logo}>
+              <img
+                src="https://new-ella.myshopify.com/cdn/shop/files/ELLA-white_230x.png?v=1733293883"
+                alt="logo"
+              />
+            </div>
+          </Link>
+
+          {/* Search Input */}
+          <div className={style.search}>
+            <input
+              type="search"
+              name="search"
+              id="search"
+              placeholder="Search"
+            />
+          </div>
+          <div className="flex items-center gap-5">
+            {/* Right Links*/}
+            <ul className={style.links}>
+              {RightLinks.map((link) => (
+                <li key={link.name} className={style.linksitem}>
                   <Link to={link.url}>{link.name}</Link>
                 </li>
               ))}
             </ul>
-          </div>
-          <div className={style.logo}>
-            <img
-              src="https://new-ella.myshopify.com/cdn/shop/files/ELLA-white_230x.png?v=1733293883"
-              alt="logo"
-            />
+            <select className={style.languageSelect}>
+              <option value="en">EN</option>
+              <option value="az">AZ</option>
+            </select>
           </div>
         </div>
       </nav>
